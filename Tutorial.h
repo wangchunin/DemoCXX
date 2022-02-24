491b65e5db33d0079fe4e68675f218cf900cc771
wangchunlin <wangchunin666@gmail.com>
2022-02-24 13:48:59 +0800

    加入交叉编译说明

--- Tutorial.h
+++ Tutorial.h
@@ -1,4 +1,5 @@
 // the configured options and settings for Tutorial
 #define Tutorial_VERSION_MAJOR 5
 #define Tutorial_VERSION_MINOR 0
-/* #undef USE_MYMATH */
+// 必须加入-DUSE_MYMATH才能在.h文件中生成对应宏，或者执行两次cmake也可以生成此宏
+#define USE_MYMATH
